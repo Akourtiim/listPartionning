856315eda2856e9ba4f867c689600a59c25aebe9
ahmed <akourtim.ahmed@gmail.com>
2020-10-10 18:48:54 +0200

    First commit

---
+++ partionner/src/main/java/com/lib/Partionner.java
@@ -0,0 +1,28 @@
+package com.lib;
+
+import java.util.Arrays;
+
+/**
+ * @author AKOURTIM Ahmed on 2020-10-10
+ */
+public class Partionner {
+
+    public static void main(String[] args){
+
+        try {
+
+            String list = args[0];
+            String partionSize = args[1];
+        }catch (IndexOutOfBoundsException ioobe){
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ partionner/src/main/java/com/lib/PartionnerManager.java
@@ -0,0 +1,44 @@
+package com.lib;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
+/**
+ * @author AKOURTIM Ahmed on 2020-10-10
+ */
+public class PartionnerManager {
+
+    public static List<List<Integer>> partition(List<Integer> list,int partionSize) throws Exception{
+
+            if(list==null || list.size()==0 || partionSize <=0){
+                throw new Exception("list is empty or null or the partition size is null or negative");
+            }
+
+            if(list.size()<partionSize){
+                throw new Exception("Size of partition must be not be greater the list size");
+            }
+
+            if(list.size()==partionSize){
+                List<List<Integer>> defaultPartion = new ArrayList<>();
+                defaultPartion.add(list);
+                return defaultPartion;
+            }
+
+
+            final AtomicInteger indexCouneter = new AtomicInteger();
+
+            final Collection<List<Integer>> partions=list.stream().collect(Collectors.groupingBy
+                    (it->indexCouneter.getAndIncrement()/ partionSize)).values();
+
+
+        ArrayList<List<Integer>> finalList = new ArrayList<>(partions);
+
+
+       return finalList;
+    }
+}
